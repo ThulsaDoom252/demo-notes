1be62cd299aba2004a7d1e1cf172a023267119e5
ThulsaDoom252 <xenolm252@gmail.com>
2022-07-11 23:43:09 +0300

    add Note component(Ui only)

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import homeStyle from "./Home.module.css"
 import {FiFilePlus} from "react-icons/fi";
 import EditWindow from "../../components/editwindow/EditWindow";
+import Notes from "../../components/notes/Notes";
 
 const Home = (props) => {
     const [write, writeMode] = useState(false)
@@ -10,6 +11,7 @@ const Home = (props) => {
             <button className={homeStyle.button} onClick={() => writeMode(true)}>Add new note <FiFilePlus/></button>
             <EditWindow show = {{write, writeMode}}/>
         <hr className={homeStyle.line}/>
+            <Notes/>
         </div>
     );
 }
